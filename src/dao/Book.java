24357bb0c680dcd8614b17afe85cb855014c20b3
santoshtula123 <santoshtuladhar26@gmail.com>
2025-12-29 20:13:23 +0545

    Added code

---
+++ src/dao/Book.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+
+/**
+ *
+ * @author ASUS
+ */
+public class Book {
+    public int id;
+    public String name;
+    public String type;
+    public String imagePath;
+    public String category;
+    
+    
+    
+}
