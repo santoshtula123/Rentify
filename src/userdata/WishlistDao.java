cd96316a23218446256b26169c684b02b3a65efe
santoshtula123 <santoshtuladhar26@gmail.com>
2025-12-31 12:56:35 +0545

    updates made to product category

--- src/userdata/WishlistDao.java
+++ src/userdata/WishlistDao.java
@@ -29,12 +29,17 @@ public class WishlistDao {
                 PreparedStatement pst = con.prepareStatement(sql)) {
             pst.setInt(1, userId);
             pst.setInt(2, productId);
+<<<<<<< HEAD
             return pst.executeUpdate() > 0;
+=======
+            return pst.executeUpdate() == 1;
+>>>>>>> 818be83 (updates made to product category)
         }
     }
     
     public boolean removeFromWishlist(int userId, int productId) throws SQLException {
         String sql = "DELETE FROM wishlist WHERE user_id = ? AND product_id = ?";
+<<<<<<< HEAD
     try (Connection con = mysql.openConnection();
          PreparedStatement pst = con.prepareStatement(sql)) {
         pst.setInt(1, userId);
@@ -44,6 +49,15 @@ public class WishlistDao {
         return rows > 0;   // <-- change from == 1 to > 0
     }
 }
+=======
+        try (Connection con = mysql.openConnection();
+                PreparedStatement pst = con.prepareStatement(sql)) {
+            pst.setInt(1, userId);
+            pst.setInt(2, productId);
+            return pst.executeUpdate() == 1;
+        }
+    }
+>>>>>>> 818be83 (updates made to product category)
     public  List<ProductModel> getWishlist(int userId) throws SQLException {
         List<ProductModel> list = new ArrayList<>();
         String sql = """
